2dac8f99431eb9c83dd9877a3db890fb086891dd
Avraham Gidon <workshandesai19@gmail.com>
2023-01-15 15:27:49 +0200

    first test in class

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -3,11 +3,13 @@ package com.example.testgit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Avraham Gidon ID: 325533347 date: 15/01/2023
+ */
 @SpringBootApplication
 public class TestGitApplication {
 
     public static void main(String[] args) {
-
         System.out.print("Hello World");
     }
 
