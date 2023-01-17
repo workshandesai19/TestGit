ac1a8bd3ab19d2808ef7b2b23bbbb74ced6aab25
Avraham Gidon <workshandesai19@gmail.com>
2023-01-17 08:16:41 +0200

    The SQL code

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -3,6 +3,8 @@ package com.example.testgit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 /**
  * @author Avraham Gidon ID: 325533347 date: 15/01/2023
  */
@@ -10,10 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestGitApplication {
 
     public static void main(String[] args) {
-        System.out.print("sharel hahatih");
-        System.out.print("avram ya naknik! ");
-        System.out.print("mrmita");
+        System.out.println("sharel hahatih");
+        System.out.println("avram ya naknik! ");
+        System.out.println("mrmita");
         System.out.println("lol, not funny!!!!!!");
+        connectPostgreSQL co = new connectPostgreSQL();
+        co.select();
+//        connectPostgreSQL db = new connectPostgreSQL();
+//        Connection asd = db.connect_to_db("postgres","postgres","1234");
+
     }
 
 }
